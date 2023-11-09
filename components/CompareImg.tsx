7e0af5a948098f1fec11d6d47690e9716d666704
Vishnu V S <vishnusenthil222@gmail.com>
2023-11-09 17:08:48 +0530

    error fixed

--- components/CompareImg.tsx
+++ components/CompareImg.tsx
@@ -1,24 +1,31 @@
-"use client"
 import React from "react";
 import { ImgComparisonSlider } from "@acf-comparison-slider/react";
 
-export default function CompareImg({logImg,clrdImg}) {
+// Define the prop types using TypeScript
+interface MyComponentProps {
+  logImg: string; // Fix: Add type for logImg
+  clrdImg: string; // Fix: Add type for clrdImg
+}
+
+const MyComponent: React.FC<MyComponentProps> = ({ logImg, clrdImg }) => {
   return (
-    <div className="h-full w-fulll outline-none" style={{outline:'none'}}>
-      <ImgComparisonSlider  style={{outline:'none',borderRadius:'6px',height:'100%',width:'100%',objectFit:'contain'}}>
+    <div className="h-full w-full outline-none" style={{ outline: 'none' }}>
+      <ImgComparisonSlider style={{ outline: 'none', borderRadius: '6px', height: '100%', width: '100%', objectFit: 'contain' }}>
         <img
           slot="first"
           alt="first"
-          style={{height:'100%',width:'100%',borderRadius:'6px'}}
+          style={{ height: '100%', width: '100%', borderRadius: '6px' }}
           src={logImg}
         />
         <img
           slot="second"
           alt="second"
-          style={{height:'100%',width:'100%',borderRadius:'6px'}}
+          style={{ height: '100%', width: '100%', borderRadius: '6px' }}
           src={clrdImg}
         />
       </ImgComparisonSlider>
     </div>
   );
 }
+
+export default MyComponent;
