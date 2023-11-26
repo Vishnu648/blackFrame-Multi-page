3337a61ccfd73f1ba8f11b5e0d279206d28950c3
Vishnu V S <96856622+Vishnu648@users.noreply.github.com>
2023-11-26 14:28:12 +0530

    Update what we do page

--- app/whatWeDo/page.tsx
+++ app/whatWeDo/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Link from 'next/link'
 
 export default function page() {
   return (
     <div className="flex flex-col text-[#2a2a2a] p-4 bg-[#e9ecef] w-full" style={{ fontFamily: "myCustomFont",letterSpacing:'1px'}}>
       <div className="w-full">
         <div className="flex items-center ">
+         <Link className="hover:text-gray-500" href='/'>
           <img
             src="/onlyLogo.png"
             alt="Logo"
             width={100}
             className=" flex items-center justify-center text-left"
           />
+          </Link>
 
           <h1 style={{ fontFamily: "myCustomFont" }} className="text-3xl">
             BLACKFRAME PICTURES
